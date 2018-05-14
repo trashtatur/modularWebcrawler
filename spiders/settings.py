e99f6d10546396c33e96e8e49d24b616339f68fa
Andreas Hörster <andreas-hoerster@web.de>
2018-05-14 08:52:09 +0200

    Changed logging level to info

--- spiders/settings.py
+++ spiders/settings.py
@@ -4,6 +4,7 @@ custom_settings = {
     "NEWSPIDER_MODULE": 'webcrawler.spiders',
     "USER_AGENT": 'webcrawler (www.yourcoon.de) by HAW Hamburg germany',
     "ROBOTSTXT_OBEY": True,
+    "LOG_LEVEL": 'INFO',
     "DOWNLOAD_DELAY": 2,
     "ITEM_PIPELINES": {
         #'webcrawler.pipelines.RabbitMQPipeline':100
